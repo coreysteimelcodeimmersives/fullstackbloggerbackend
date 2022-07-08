7af5a6e7cbb5aedf7622339c93a3fc6f7e5a0a38
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-07-08 09:13:38 -0600

    finished

--- routes/blogs.js
+++ routes/blogs.js
@@ -12,7 +12,7 @@ router.get("/all-blogs", async (req, res) => {
     const limit = Number(req.query.limit);
     const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
     const sortField = req.query.sortField;
-    const sortOrder = req.query.sortOrder === "ASC" ? Number(1) : Number(-1);
+    const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
     const filterField = req.query.filterField;
     const filterValue = req.query.filterValue;
     const collection = await blogsDB().collection("posts50");
@@ -24,8 +24,6 @@ router.get("/all-blogs", async (req, res) => {
     if (sortField && sortOrder) {
       sortObj = { [sortField]: sortOrder };
     }
-    console.log("filterobj", filterObj);
-    console.log("sortObj", sortObj);
     const posts = await collection
       .find(filterObj)
       .sort(sortObj)
